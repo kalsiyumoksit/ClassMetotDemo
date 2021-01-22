ed453af6906c6b76590f2206d5c28959358c1600
Tugba <72036165+kalsiyumoksit@users.noreply.github.com>
2021-01-22 16:04:45 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " isimli müşteri eklendi.");
+        }
+        public void Guncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " isimli müşteri bilgileri güncellendi.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " isimli müşteri bilgileri silindi.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriId + " " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " " + musteri.MusteriDogumTarihi + "-->" + " müşteri bilgileri listelendi");
+        }
+        public void MevcutMusteriListele(Musteri[] musteri)
+        {
+            foreach (Musteri musterii in musteri)
+            {
+                Console.WriteLine(musterii.MusteriId);
+                Console.WriteLine(musterii.MusteriAdi);
+                Console.WriteLine(musterii.MusteriSoyadi);
+                Console.WriteLine(musterii.MusteriDogumTarihi);
+
+            }
+        }
+        public void Cizgi()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("-*-*--*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine(" ");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bankada müşteri takibi yapmak
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAdi = "Cengiz";
+            musteri1.MusteriSoyadi = "Aytmatov";
+            musteri1.MusteriDogumTarihi = 1928;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriAdi = "Ahmet";
+            musteri2.MusteriSoyadi = "Ümit";
+            musteri2.MusteriDogumTarihi = 1960;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 3;
+            musteri3.MusteriAdi = "Ahmet Hamdi";
+            musteri3.MusteriSoyadi = "Tanpınar";
+            musteri3.MusteriDogumTarihi = 1901;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MevcutMusteriListele(musteriler);
+            musteriManager.Cizgi();
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Cizgi();
+            musteriManager.Ekle(musteri3);
+            musteriManager.Cizgi();
+            musteriManager.Guncelle(musteri2);
+            musteriManager.Cizgi();
+            musteriManager.Sil(musteri1);
+
+        }
+    }
+}
